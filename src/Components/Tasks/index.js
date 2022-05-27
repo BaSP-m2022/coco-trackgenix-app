3ac84dade08bf09a85a88b7564dd10cc3efac2d5
LucasMattia <mattia.114.99@gmail.com>
2022-05-27 20:50:01 -0300

    TG-79: app connected to server

--- src/Components/Tasks/index.js
+++ src/Components/Tasks/index.js
@@ -1,11 +1,21 @@
+import React, { useEffect } from 'react';
 import styles from './tasks.module.css';
 
-function Tasks() {
+const Tasks = () => {
+  useEffect(async () => {
+    try {
+      const response = await fetch(`https://coco-trackgenix-server.vercel.app/tasks`);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
   return (
     <section className={styles.container}>
-      <h2>Tasks</h2>
+      <h2 className>Tasks</h2>
     </section>
   );
-}
+};
 
 export default Tasks;
