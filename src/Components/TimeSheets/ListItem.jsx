7eeaf90a5961776e7ff43c8432faada179f6fcae
Dayana Guerra <dayana96gm@gmail.com>
2022-05-30 15:17:46 -0300

    BaSP-TG-80: Create TableList, ListItem and Form components and their functionalities and edit index.js

---
+++ src/Components/TimeSheets/ListItem.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const ListItem = ({ listItem, deleteItem }) => {
+  const handleDelete = () => {
+    deleteItem(listItem._id);
+  };
+
+  const tasks = listItem.tasks;
+  const stDate = new Date(listItem.startDate).toLocaleDateString();
+  const enDate = new Date(listItem.endDate).toLocaleDateString();
+  return (
+    <tr>
+      <td>
+        <ul>
+          {tasks.map((element) => {
+            return <li key={element.id}>{element.description.toUpperCase()}</li>;
+          })}
+        </ul>
+      </td>
+      <td>{listItem.employeeId.firstName}</td>
+      <td>{listItem.projectId != null ? listItem.projectId.name : 'not assigned!'}</td>
+      <td>{`${stDate}`}</td>
+      <td>{`${enDate}`}</td>
+      <td>
+        <button onClick={() => handleDelete(listItem._id)}>X</button>
+      </td>
+      <td>
+        <button>Edit</button>
+      </td>
+    </tr>
+  );
+};
+
+export default ListItem;
--- src/Components/TimeSheets/index.js
+++ src/Components/TimeSheets/index.js
@@ -1,11 +1,66 @@
 import styles from './time-sheets.module.css';
+import List from './TableList';
+import React, { useState, useEffect } from 'react';
+import Form from './Form';
 
 function TimeSheets() {
-  return (
-    <section className={styles.container}>
-      <h2>TimeSheets</h2>
-    </section>
-  );
+  let [change, setSwitch] = useState(false);
+  const [list, setList] = useState([]);
+  useEffect(() => {
+    fetch(`https://coco-trackgenix-server.vercel.app/timesheets`)
+      .then((res) => res.json())
+      .then((json) => {
+        console.log('data', json.data);
+        setList(json.data);
+      });
+  }, []);
+
+  const deleteItem = (id) => {
+    console.log(id);
+    setList([...list.filter((listItem) => listItem._id !== id)]);
+    try {
+      const response = fetch(`https://coco-trackgenix-server.vercel.app/timesheets/${id}`, {
+        method: 'DELETE'
+      });
+      console.log(response);
+      alert('The time-sheet has been deleted successfully');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const switcher = () => {
+    setSwitch(change ? (change = false) : (change = true));
+  };
+
+  // const addItem = ({ tasks, employeeId, projectId, startDate, endDate }) => {
+  //   const newItem = {
+  //     id: Math.floor(Math.random() * 1000),
+  //     tasks,
+  //     employeeId,
+  //     projectId,
+  //     startDate,
+  //     endDate
+  //   };
+  //   setList([...list, newItem]);
+  // };
+  if (change) {
+    return (
+      <section className={styles.container}>
+        <h2>TimeSheets</h2>
+        <button onClick={switcher}>back</button>
+        <Form switcher={switcher} />
+      </section>
+    );
+  } else {
+    return (
+      <section className={styles.container}>
+        <h2>TimeSheets</h2>
+        <button onClick={switcher}>Add new</button>
+        <List list={list} setList={setList} deleteItem={deleteItem} />
+      </section>
+    );
+  }
 }
 
 export default TimeSheets;
