71f573374e68728fd7d3d127c96196fbfdd5d6f0
Dayana Guerra <dayana96gm@gmail.com>
2022-06-03 12:11:37 -0300

    BaSP-TG-80: fixing date format

--- src/Components/TimeSheets/ListItem.jsx
+++ src/Components/TimeSheets/ListItem.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 
 const ListItem = ({ listItem, deleteItem }) => {
+  const dateFormatter = (inputDate) => {
+    const date = new Date(inputDate);
+
+    const day = date.getDate();
+    const month = date.getMonth();
+    const year = date.getFullYear();
+
+    return `${day}/${month}/${year}`;
+  };
   const timeSheetList = listItem.map((item) => {
     return (
       <tr key={item._id}>
         <td>{item.employeeId.firstName}</td>
         <td>{item.projectId != null ? item.projectId.name : 'No project'}</td>
-        <td>{item.startDate.toString()}</td>
-        <td>{item.endDate.toString()}</td>
+        <td>{dateFormatter(item.startDate.substring(0, 10))}</td>
+        <td>{dateFormatter(item.endDate.substring(0, 10))}</td>
         <td>{item.tasks.length === 1 ? item.tasks[0].description : 'Various Task'}</td>
         <td>
           <button onClick={() => deleteItem(item._id)}>X</button>
