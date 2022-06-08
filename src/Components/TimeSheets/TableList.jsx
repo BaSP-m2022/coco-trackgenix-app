36711c39d69b7896495bc01df944e9a2829e422b
Marco Migliaro <marcomig96@gmail.com>
2022-06-07 22:54:25 -0300

    BaSP-TG-101: Initial commit for implementing shared components

--- src/Components/TimeSheets/TableList.jsx
+++ src/Components/TimeSheets/TableList.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ListItem from './ListItem';
 
 const TableList = ({ list, deleteItem, editMode, updateItem }) => {
+  // console.log('added modals');
   return (
     <div>
       <table>
