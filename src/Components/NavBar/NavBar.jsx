0ab775c135a883f6dca9b71f2c8313d892f7c66b
SuarezAlexisEzequiel <alexsuarezproyecto@gmail.com>
2022-06-07 23:25:44 -0300

    TG-90: Added feature to NavBar to show it on every page except home and nav

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -4,36 +4,40 @@ import userIcon from '../../Assets/user.svg';
 import styles from './navBar.module.css';
 
 const NavBar = (props) => {
-  return (
-    <nav className={styles.navbar}>
-      <div className={styles.userInfo}>
-        <img src={userIcon} className={styles.userImg} />
-        <div className={styles.userName}>User Name</div>
-        <div className={styles.userRole}>User Role</div>
-      </div>
-      <div className={styles.menu}>Menu</div>
-      <ul className={styles.rutes}>
-        <li>
-          <button onClick={() => props.history.push('/projects/')}>projects</button>
-        </li>
-        <li>
-          <button onClick={() => props.history.push('/employees/')}>employees</button>
-        </li>
-        <li>
-          <button onClick={() => props.history.push('/time-sheets/')}>timesheets</button>
-        </li>
-        <li>
-          <button onClick={() => props.history.push('/tasks/')}>tasks</button>
-        </li>
-        <li>
-          <button onClick={() => props.history.push('/admins/')}>admins</button>
-        </li>
-        <li>
-          <button onClick={() => props.history.push('/super-admins/')}>super admin</button>
-        </li>
-      </ul>
-    </nav>
-  );
+  if (window.location.pathname == '/home' || window.location.pathname == '/nav') {
+    return null;
+  } else {
+    return (
+      <nav className={styles.navbar}>
+        <div className={styles.userInfo}>
+          <img src={userIcon} className={styles.userImg} />
+          <div className={styles.userName}>User Name</div>
+          <div className={styles.userRole}>User Role</div>
+        </div>
+        <div className={styles.menu}>Menu</div>
+        <ul className={styles.rutes}>
+          <li>
+            <button onClick={() => props.history.push('/projects/')}>projects</button>
+          </li>
+          <li>
+            <button onClick={() => props.history.push('/employees/')}>employees</button>
+          </li>
+          <li>
+            <button onClick={() => props.history.push('/time-sheets/')}>timesheets</button>
+          </li>
+          <li>
+            <button onClick={() => props.history.push('/tasks/')}>tasks</button>
+          </li>
+          <li>
+            <button onClick={() => props.history.push('/admins/')}>admins</button>
+          </li>
+          <li>
+            <button onClick={() => props.history.push('/super-admins/')}>super admin</button>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
 };
 
 export default withRouter(NavBar);
