ee6cd69a7827d98679136d0310ad07c36ea445ff
Marco Migliaro <marcomig96@gmail.com>
2022-06-10 03:04:28 -0300

    BaSP-TG-101: Added table shared component

--- src/Components/TimeSheets/index.js
+++ src/Components/TimeSheets/index.js
@@ -1,16 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import styles from './time-sheets.module.css';
+import { useHistory } from 'react-router-dom';
 import Button from '../SharedComponents/Button/Button';
-// import Table from '../SharedComponents/Table';
+import Table from '../SharedComponents/Table';
 import TableList from './TableList';
 
 function TimeSheets(props) {
   const [list, setList] = useState([]);
+  // const [isOpen, setIsOpen] = useState(false);
+
+  const amountOfTasks = (tasks) => {
+    if (tasks.length === 1) {
+      return tasks[0].description;
+    } else if (tasks.length === 0) {
+      return 'Not assigned';
+    } else {
+      return 'Various Tasks';
+    }
+  };
+
+  const dateFormatter = (inputDate) => {
+    const date = new Date(inputDate);
+
+    const day = date.getDate();
+    const month = date.getMonth();
+    const year = date.getFullYear();
+
+    return `${month}/${day}/${year}`;
+  };
 
   useEffect(() => {
     fetch(`https://coco-trackgenix-server.vercel.app/timesheets`)
       .then((res) => res.json())
       .then((json) => {
+        json.data.map((item) => {
+          item.employeeId = item.employeeId ? item.employeeId.firstName : 'No Employee';
+          item.projectId = item.projectId ? item.projectId.name : 'No project';
+          item.startDate = dateFormatter(item.startDate.substring(0, 10));
+          item.endDate = dateFormatter(item.endDate.substring(0, 10));
+          item.tasks = amountOfTasks(item.tasks);
+        });
         setList(json.data);
       });
   }, []);
@@ -28,18 +57,24 @@ function TimeSheets(props) {
     }
   };
 
+  let history = useHistory();
+  const handleEdit = (item) => {
+    history.push(`/time-sheets/edit?=${item}`);
+  };
+
   return (
     <section className={styles.container}>
       <h2 className={styles.title}>TimeSheets</h2>
-      {/* <Table
+      <Table
         data={list}
-        headers={['employeeId.firstName', 'projectId.name', 'startDate', 'endDate', 'tasks']}
+        headers={['employeeId', 'projectId', 'startDate', 'endDate', 'tasks']}
+        handleEdit={handleEdit}
         deleteItem={deleteItem}
       >
         <Button type={styles.addBtn} handleClick={() => props.history.push('/time-sheets/add')}>
           Add Time Sheet
         </Button>
-      </Table> */}
+      </Table>
       <Button type={styles.addBtn} handleClick={() => props.history.push('/time-sheets/add')}>
         Add Time Sheet
       </Button>
