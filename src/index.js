778a5788e9b9f9dcea8f9e400dfcb3bef0421d4f
Marco Migliaro <marcomig96@gmail.com>
2022-06-10 14:11:54 -0300

    BaSP-TG-129: Installed redux dev tools and created redux store

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import Layout from './Components/Layout';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import store from './Components/redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
