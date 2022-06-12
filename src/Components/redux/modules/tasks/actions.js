a3aca6769748946f99db974f7e9ff63f26f3aa67
Marco Migliaro <marcomig96@gmail.com>
2022-06-12 00:34:40 -0300

    BaSP-TG-133: Created all actions and constants for tasks

--- src/Components/redux/modules/tasks/actions.js
+++ src/Components/redux/modules/tasks/actions.js
@@ -1,16 +1,100 @@
-import { ADD_TASKS_FULFILLED, DELETE_TASKS_FULFILLED, GET_TASKS_FULFILLED } from './constants';
+import {
+  GET_TASKS_PENDING,
+  GET_TASKS_SUCCESS,
+  GET_TASKS_ERROR,
+  ADD_TASKS_PENDING,
+  ADD_TASKS_SUCCESS,
+  ADD_TASKS_ERROR,
+  DELETE_TASKS_PENDING,
+  DELETE_TASKS_SUCCESS,
+  DELETE_TASKS_ERROR,
+  EDIT_TASKS_PENDING,
+  EDIT_TASKS_SUCCESS,
+  EDIT_TASKS_ERROR
+} from './constants';
 
-export const getTasksFulfilled = (tasks) => ({
-  type: GET_TASKS_FULFILLED,
-  payload: tasks
-});
+export const getTasksPending = () => {
+  return {
+    type: GET_TASKS_PENDING
+  };
+};
 
-export const addTasksFulfilled = (task) => ({
-  type: ADD_TASKS_FULFILLED,
-  payload: task
-});
+export const getTasksSuccess = (data) => {
+  return {
+    type: GET_TASKS_SUCCESS,
+    payload: data
+  };
+};
 
-export const DeleteTasksFulfilled = (taskId) => ({
-  type: DELETE_TASKS_FULFILLED,
-  payload: taskId
-});
+export const getTasksError = (error) => {
+  return {
+    type: GET_TASKS_ERROR,
+    payload: error
+  };
+};
+
+// ADD
+
+export const addTasksPending = () => {
+  return {
+    type: ADD_TASKS_PENDING
+  };
+};
+
+export const addTasksSuccess = (data) => {
+  return {
+    type: ADD_TASKS_SUCCESS,
+    payload: data
+  };
+};
+
+export const addTasksError = (error) => {
+  return {
+    type: ADD_TASKS_ERROR,
+    payload: error
+  };
+};
+
+// DELETE
+
+export const deleteTasksPending = () => {
+  return {
+    type: DELETE_TASKS_PENDING
+  };
+};
+
+export const deleteTasksSuccess = (dataId) => {
+  return {
+    type: DELETE_TASKS_SUCCESS,
+    payload: dataId
+  };
+};
+
+export const deleteTasksError = (error) => {
+  return {
+    type: DELETE_TASKS_ERROR,
+    payload: error
+  };
+};
+
+// EDIT
+
+export const editTasksPending = () => {
+  return {
+    type: EDIT_TASKS_PENDING
+  };
+};
+
+export const editTasksSuccess = (dataId) => {
+  return {
+    type: EDIT_TASKS_SUCCESS,
+    payload: dataId
+  };
+};
+
+export const editTasksError = (error) => {
+  return {
+    type: EDIT_TASKS_ERROR,
+    payload: error
+  };
+};
