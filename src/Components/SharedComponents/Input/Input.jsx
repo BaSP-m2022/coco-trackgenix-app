70febf6075b0cb7aa0bd6346a15d9ec1bd58d30d
Lucas De Lorenzi <lucasrauldelorenzi@gmail.com>
2022-06-12 19:45:45 -0300

    BaSP-TG-126: Create Input shared component

---
+++ src/Components/SharedComponents/Input/Input.jsx
@@ -0,0 +1,33 @@
+import styles from './input.module.css';
+
+const Input = ({
+  labelText,
+  name,
+  inputValue,
+  placeholder,
+  warningMsg,
+  handleInput,
+  handleClick,
+  handleBlur,
+  showWarning
+}) => {
+  return (
+    <div className={styles.inputContainer}>
+      <label className={styles.label} htmlFor={name}>
+        {labelText}
+      </label>
+      <input
+        className={showWarning ? styles.emptyInput : styles.input}
+        type="text"
+        name={name}
+        value={inputValue}
+        placeholder={placeholder}
+        onInput={handleInput}
+        onClick={handleClick}
+        onBlur={handleBlur}
+      ></input>
+      <p className={showWarning ? styles.showWarningMsg : styles.hideWarningMsg}>{warningMsg}</p>
+    </div>
+  );
+};
+export default Input;
