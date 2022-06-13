19fb558f405cdc083058b24d3b71196f490e91d1
Dayana Guerra <dayana96gm@gmail.com>
2022-06-12 23:42:17 -0300

    BaSP-TG-132: reducers for superAdmins

--- src/Components/redux/rootReducer.js
+++ src/Components/redux/rootReducer.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
+import { superAdminsReducer } from './modules/superAdmins/reducer';
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({ superAdmin: superAdminsReducer });
 
 export default rootReducer;
