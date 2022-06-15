b1d326c3de613aac3453f87c95b316942ece109d
gregorioelias <gregorio.elias24@gmail.com>
2022-06-15 05:34:33 -0300

    TG-131: added shared components inputs on forms

--- src/Components/Admins/AdminForm/AdminForm.jsx
+++ src/Components/Admins/AdminForm/AdminForm.jsx
@@ -4,25 +4,35 @@ import Button from '../../SharedComponents/Button/Button';
 import Modal from '../../SharedComponents/Modal/Modal';
 import Logo from '../../SharedComponents/Logo/Logo';
 import Dropdown from '../../SharedComponents/Dropdown/Dropdown';
+import Input from '../../SharedComponents/Input/Input';
 import { useDispatch } from 'react-redux';
 import { postAdmin } from '../../redux/modules/admins/thunks';
 
 const AdminForm = (props) => {
   const [isOpen, setIsOpen] = useState(false);
   const [adminInput, setAdminInput] = useState({
-    name: '',
-    lastName: '',
-    email: '',
-    password: '',
+    name: props.name,
+    lastName: props.lastName,
+    email: props.email,
+    password: props.password,
     active: ''
   });
+
+  const [showWarning1, setShowWarning1] = useState(false);
+  const [showWarning2, setShowWarning2] = useState(false);
+  const [showWarning3, setShowWarning3] = useState(false);
+  const [showWarning4, setShowWarning4] = useState(false);
+
   const backAdmin = () => {
     props.history.push('/admins');
   };
+
   const onChange = (e) => {
     setAdminInput({ ...adminInput, [e.target.name]: e.target.value });
   };
+
   const dispatch = useDispatch();
+
   const formAdmin = (e) => {
     dispatch(postAdmin(e, setIsOpen, backAdmin));
     console.log('ACA ESTTOY', e);
@@ -38,6 +48,94 @@ const AdminForm = (props) => {
     });
   }
 
+  const handleInput1 = (e) => {
+    setAdminInput({
+      ...adminInput,
+      [e.target.name]: e.target.value
+    });
+    if (e.target.value === '') {
+      setShowWarning1(true);
+    } else {
+      setShowWarning1(false);
+    }
+  };
+
+  const handleInput2 = (e) => {
+    setAdminInput({
+      ...adminInput,
+      [e.target.name]: e.target.value
+    });
+    if (e.target.value === '') {
+      setShowWarning2(true);
+    } else {
+      setShowWarning2(false);
+    }
+  };
+
+  const handleInput3 = (e) => {
+    setAdminInput({
+      ...adminInput,
+      [e.target.name]: e.target.value
+    });
+    if (e.target.value === '') {
+      setShowWarning3(true);
+    } else {
+      setShowWarning3(false);
+    }
+  };
+
+  const handleInput4 = (e) => {
+    setAdminInput({
+      ...adminInput,
+      [e.target.name]: e.target.value
+    });
+    if (e.target.value === '') {
+      setShowWarning4(true);
+    } else {
+      setShowWarning4(false);
+    }
+  };
+
+  const handleClick1 = () => {
+    setShowWarning1(false);
+  };
+
+  const handleClick2 = () => {
+    setShowWarning2(false);
+  };
+
+  const handleClick3 = () => {
+    setShowWarning3(false);
+  };
+
+  const handleClick4 = () => {
+    setShowWarning4(false);
+  };
+
+  const handleBlurInput1 = (e) => {
+    if (e.target.value === '') {
+      setShowWarning1(true);
+    }
+  };
+
+  const handleBlurInput2 = (e) => {
+    if (e.target.value === '') {
+      setShowWarning2(true);
+    }
+  };
+
+  const handleBlurInput3 = (e) => {
+    if (e.target.value === '') {
+      setShowWarning3(true);
+    }
+  };
+
+  const handleBlurInput4 = (e) => {
+    if (e.target.value === '') {
+      setShowWarning4(true);
+    }
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.logo}>
@@ -47,26 +145,57 @@ const AdminForm = (props) => {
       <form onSubmit={onSubmit} className={styles.formContainer}>
         <div>
           <div>
-            <label>Name</label>
-            <input type="text" name="name" value={adminInput.name} onChange={onChange} />
+            <Input
+              labelText="Name"
+              name="name"
+              inputValue={adminInput.name}
+              placeholder="Name"
+              warningMsg="Please check the information"
+              handleInput={handleInput1}
+              handleClick={handleClick1}
+              handleBlur={handleBlurInput1}
+              showWarning={showWarning1}
+            />
           </div>
           <div>
-            <label>Last Name</label>
-            <input type="text" name="lastName" value={adminInput.lastName} onChange={onChange} />
+            <Input
+              labelText="Last Name"
+              name="lastName"
+              inputValue={adminInput.lastName}
+              placeholder="Last Name"
+              warningMsg="Please check the information"
+              handleInput={handleInput2}
+              handleClick={handleClick2}
+              handleBlur={handleBlurInput2}
+              showWarning={showWarning2}
+            />
           </div>
         </div>
         <div>
           <div>
-            <label>Email</label>
-            <input type="text" name="email" value={adminInput.email} onChange={onChange} />
+            <Input
+              labelText="Email"
+              name="email"
+              inputValue={adminInput.email}
+              placeholder="Email"
+              warningMsg="Please check the information"
+              handleInput={handleInput3}
+              handleClick={handleClick3}
+              handleBlur={handleBlurInput3}
+              showWarning={showWarning3}
+            />
           </div>
           <div>
-            <label>Password</label>
-            <input
-              type="password"
+            <Input
+              labelText="Password"
               name="password"
-              value={adminInput.password}
-              onChange={onChange}
+              inputValue={adminInput.password}
+              placeholder="Password"
+              warningMsg="Please check the information"
+              handleInput={handleInput4}
+              handleClick={handleClick4}
+              handleBlur={handleBlurInput4}
+              showWarning={showWarning4}
             />
           </div>
         </div>
--- src/Components/Admins/AdminForm/AdminFormEdit.jsx
+++ src/Components/Admins/AdminForm/AdminFormEdit.jsx
@@ -4,6 +4,7 @@ import Button from '../../SharedComponents/Button/Button';
 import Modal from '../../SharedComponents/Modal/Modal';
 import Logo from '../../SharedComponents/Logo/Logo';
 import Loading from '../../SharedComponents/Loading/Loading';
+import Input from '../../SharedComponents/Input/Input';
 import Dropdown from '../../SharedComponents/Dropdown/Dropdown';
 import { useDispatch, useSelector } from 'react-redux';
 import { putAdmin, getAdminById } from '../../redux/modules/admins/thunks';
@@ -18,7 +19,12 @@ const AdminFormEdit = (props) => {
   const [passwordInput, setPasswordInput] = useState('');
   const [activeInput, setActiveInput] = useState('');
   const [adminEdit, setAdminEdit] = useState({});
+  const [showWarning1, setShowWarning1] = useState(false);
+  const [showWarning2, setShowWarning2] = useState(false);
+  const [showWarning3, setShowWarning3] = useState(false);
+  const [showWarning4, setShowWarning4] = useState(false);
   const params = window.location.search;
+
   let id = params.substring(2);
   const backAdmin = () => {
     props.history.push('/admins');
@@ -55,7 +61,81 @@ const AdminFormEdit = (props) => {
   if (isLoading) {
     return <Loading className={styles.loading}></Loading>;
   }
+  const handleInput1 = (e) => {
+    setNameInput(e.target.value);
+    if (e.target.value === '') {
+      setShowWarning1(true);
+    } else {
+      setShowWarning1(false);
+    }
+  };
 
+  const handleInput2 = (e) => {
+    setLastNameInput(e.target.value);
+    if (e.target.value === '') {
+      setShowWarning2(true);
+    } else {
+      setShowWarning2(false);
+    }
+  };
+
+  const handleInput3 = (e) => {
+    setEmailInput(e.target.value);
+    if (e.target.value === '') {
+      setShowWarning3(true);
+    } else {
+      setShowWarning3(false);
+    }
+  };
+
+  const handleInput4 = (e) => {
+    setPasswordInput(e.target.value);
+    if (e.target.value === '') {
+      setShowWarning4(true);
+    } else {
+      setShowWarning4(false);
+    }
+  };
+
+  const handleClick1 = () => {
+    setShowWarning1(false);
+  };
+
+  const handleClick2 = () => {
+    setShowWarning2(false);
+  };
+
+  const handleClick3 = () => {
+    setShowWarning3(false);
+  };
+
+  const handleClick4 = () => {
+    setShowWarning4(false);
+  };
+
+  const handleBlurInput1 = (e) => {
+    if (e.target.value === '') {
+      setShowWarning1(true);
+    }
+  };
+
+  const handleBlurInput2 = (e) => {
+    if (e.target.value === '') {
+      setShowWarning2(true);
+    }
+  };
+
+  const handleBlurInput3 = (e) => {
+    if (e.target.value === '') {
+      setShowWarning3(true);
+    }
+  };
+
+  const handleBlurInput4 = (e) => {
+    if (e.target.value === '') {
+      setShowWarning4(true);
+    }
+  };
   return (
     <div className={styles.container}>
       <div className={styles.logo}>
@@ -66,40 +146,56 @@ const AdminFormEdit = (props) => {
         <form onSubmit={handleSubmit} className={styles.formContainer}>
           <div>
             <div>
-              <label htmlFor="name">Name</label>
-              <input
-                type="text"
+              <Input
+                labelText="Name"
                 name="name"
-                value={nameInput}
-                onChange={(e) => setNameInput(e.target.value)}
-              ></input>
+                inputValue={nameInput}
+                placeholder="First Name"
+                warningMsg="Please check the information"
+                handleInput={handleInput1}
+                handleClick={handleClick1}
+                handleBlur={handleBlurInput1}
+                showWarning={showWarning1}
+              />
             </div>
             <div>
-              <label htmlFor="lastName">Last Name</label>
-              <input
-                type="text"
+              <Input
+                labelText="Last Name"
                 name="lastName"
-                value={lastNameInput}
-                onChange={(e) => setLastNameInput(e.target.value)}
-              ></input>
+                inputValue={lastNameInput}
+                placeholder="Last Name"
+                warningMsg="Please check the information"
+                handleInput={handleInput2}
+                handleClick={handleClick2}
+                handleBlur={handleBlurInput2}
+                showWarning={showWarning2}
+              />
             </div>
           </div>
           <div>
-            <label htmlFor="email">Email</label>
-            <input
-              type="text"
+            <Input
+              labelText="Email"
               name="email"
-              value={emailInput}
-              onChange={(e) => setEmailInput(e.target.value)}
+              inputValue={emailInput}
+              placeholder="Email"
+              warningMsg="Please check the information"
+              handleInput={handleInput3}
+              handleClick={handleClick3}
+              handleBlur={handleBlurInput3}
+              showWarning={showWarning3}
             />
           </div>
           <div>
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
+            <Input
+              labelText="Password"
               name="password"
-              value={passwordInput}
-              onChange={(e) => setPasswordInput(e.target.value)}
+              inputValue={passwordInput}
+              placeholder="Password"
+              warningMsg="Please check the information"
+              handleInput={handleInput4}
+              handleClick={handleClick4}
+              handleBlur={handleBlurInput4}
+              showWarning={showWarning4}
             />
           </div>
           <div>
