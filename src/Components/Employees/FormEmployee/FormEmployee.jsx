90a817724d8d8deab4d5a7ceb3c87f31142eee96
Lucas De Lorenzi <lucasrauldelorenzi@gmail.com>
2022-06-20 01:35:54 -0300

    BaSP-TG-141: Update and add validations with joi to employee form

--- src/Components/Employees/FormEmployee/FormEmployee.jsx
+++ src/Components/Employees/FormEmployee/FormEmployee.jsx
@@ -7,7 +7,70 @@ import Input from '../../SharedComponents/Input/Input';
 import Loading from '../../SharedComponents/Loading/Loading';
 import Dropdown from '../../SharedComponents/Dropdown/Dropdown';
 import { useDispatch, useSelector } from 'react-redux';
+import { useForm } from 'react-hook-form';
 import { addEmployee } from '../../redux/modules/employees/thunks';
+import Joi from 'joi';
+import { joiResolver } from '@hookform/resolvers/joi';
+
+const employeeSchema = Joi.object({
+  firstName: Joi.string()
+    .regex(/^([a-zA-Z]+\s)*[a-zA-Z]+$/)
+    .min(3)
+    .max(30)
+    .required()
+    .messages({
+      'string.min': 'Must contain at least 3 letters',
+      'string.max': 'Must contain a maximum of 30 letters',
+      'string.required': 'First name is required!',
+      'string.empty': 'First name is not allowed to be empty',
+      'string.pattern.base':
+        'Must contain only letters and words can only be separated by a single white space'
+    }),
+  lastName: Joi.string()
+    .regex(/^([a-zA-Z]+\s)*[a-zA-Z]+$/)
+    .min(3)
+    .max(30)
+    .required()
+    .messages({
+      'string.min': 'Must contain at least 3 letters',
+      'string.max': 'Must contain a maximum of 30 letters',
+      'string.required': 'Last name is required!',
+      'string.empty': 'Last name is not allowed to be empty',
+      'string.pattern.base':
+        'Must contain only letters and words can only be separated by a single white space'
+    }),
+  phone: Joi.string()
+    .regex(/^[0-9]+$/)
+    .min(10)
+    .max(10)
+    .messages({
+      'string.pattern.base': 'Must contain only numbers',
+      'string.min': 'Must contain 10 numbers',
+      'string.max': 'Must contain 10 numbers',
+      'string.empty': 'Phone is not allowed to be empty',
+      'number.required': 'Phone is required!'
+    }),
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .required()
+    .messages({
+      'string.email': 'Must contain an email format valid',
+      'string.empty': 'Email is not allowed to be empty',
+      'string.required': 'Email is required!'
+    }),
+  password: Joi.string()
+    .regex(/^(?=.*?\d)(?=.*?[a-zA-Z])[a-zA-Z\d]+$/)
+    .min(8)
+    .max(30)
+    .required()
+    .messages({
+      'string.min': 'Must contain at least 8 characters',
+      'string.max': 'Must contain a maximum of 30 characters',
+      'string.empty': 'Password is not allowed to be empty',
+      'string.pattern.base': 'Must contain alphanumeric characters, at least one of each',
+      'string.required': 'Password is required!'
+    })
+});
 
 const FormEmployee = (props) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -23,11 +86,12 @@ const FormEmployee = (props) => {
 
   const isLoadingEmployee = useSelector((state) => state.employee.isLoading);
 
-  const [showWarning1, setShowWarning1] = useState(false);
-  const [showWarning2, setShowWarning2] = useState(false);
-  const [showWarning3, setShowWarning3] = useState(false);
-  const [showWarning4, setShowWarning4] = useState(false);
-  const [showWarning5, setShowWarning5] = useState(false);
+  const {
+    handleSubmit,
+    register,
+    formState: { errors }
+  } = useForm({ mode: 'onChange', resolver: joiResolver(employeeSchema) });
+
   const [showButton, setShowButton] = useState(true);
   const [successEmployee, setSuccessEmployee] = useState(false);
 
@@ -36,80 +100,29 @@ const FormEmployee = (props) => {
   const formEmployee = (e) => {
     dispatch(addEmployee(e, setModalText, setShowButton, setSuccessEmployee));
     setIsOpen(true);
-    setEmployeeInput({
-      firstName: '',
-      lastName: '',
-      phone: '',
-      email: '',
-      password: '',
-      active: ''
-    });
+    if (successEmployee) {
+      setEmployeeInput({
+        firstName: '',
+        lastName: '',
+        phone: '',
+        email: '',
+        password: '',
+        active: ''
+      });
+    }
   };
 
-  const onSubmit = (e) => {
-    e.preventDefault();
+  const onSubmit = (employee) => {
+    setEmployeeInput({
+      ...employeeInput,
+      firstName: employee.firstName,
+      lastName: employee.lastName,
+      phone: employee.phone,
+      email: employee.email,
+      password: employee.password
+    });
     setModalText('Are you sure you want to create an new employee ?');
     setIsOpen(true);
-  };
-
-  const handleInput = (e) => {
-    switch (e.target.name) {
-      case 'firstName':
-        setEmployeeInput({
-          ...employeeInput,
-          [e.target.name]: e.target.value
-        });
-        if (e.target.value === '') {
-          setShowWarning1(true);
-        } else {
-          setShowWarning1(false);
-        }
-        break;
-      case 'lastName':
-        setEmployeeInput({
-          ...employeeInput,
-          [e.target.name]: e.target.value
-        });
-        if (e.target.value === '') {
-          setShowWarning2(true);
-        } else {
-          setShowWarning2(false);
-        }
-        break;
-      case 'phone':
-        setEmployeeInput({
-          ...employeeInput,
-          [e.target.name]: e.target.value
-        });
-        if (e.target.value === '') {
-          setShowWarning3(true);
-        } else {
-          setShowWarning3(false);
-        }
-        break;
-      case 'email':
-        setEmployeeInput({
-          ...employeeInput,
-          [e.target.name]: e.target.value
-        });
-        if (e.target.value === '') {
-          setShowWarning4(true);
-        } else {
-          setShowWarning4(false);
-        }
-        break;
-      case 'password':
-        setEmployeeInput({
-          ...employeeInput,
-          [e.target.name]: e.target.value
-        });
-        if (e.target.value === '') {
-          setShowWarning5(true);
-        } else {
-          setShowWarning5(false);
-        }
-        break;
-    }
   };
 
   if (isLoadingEmployee) {
@@ -122,90 +135,55 @@ const FormEmployee = (props) => {
       <div>
         <h2>Add New Employee</h2>
         <div>
-          <form onSubmit={onSubmit}>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <div>
               <Input
                 labelText="Name"
                 name="firstName"
-                inputValue={employeeInput.firstName}
+                type="text"
                 placeholder="First Name"
-                warningMsg="This field must be completed!"
-                handleInput={handleInput}
-                handleClick={() => {
-                  setShowWarning1(false);
-                }}
-                handleBlur={(e) => {
-                  if (e.target.value === '') setShowWarning1(true);
-                }}
-                showWarning={showWarning1}
+                register={register}
+                error={errors.firstName?.message}
               />
             </div>
             <div>
               <Input
                 labelText="Last Name"
                 name="lastName"
-                inputValue={employeeInput.lastName}
+                type="text"
                 placeholder="Last Name"
-                warningMsg="This field must be completed!"
-                handleInput={handleInput}
-                handleClick={() => {
-                  setShowWarning2(false);
-                }}
-                handleBlur={(e) => {
-                  if (e.target.value === '') setShowWarning2(true);
-                }}
-                showWarning={showWarning2}
+                register={register}
+                error={errors.lastName?.message}
               />
             </div>
             <div>
               <Input
                 labelText="Phone"
                 name="phone"
-                inputValue={employeeInput.phone}
+                type="text"
                 placeholder="Phone"
-                warningMsg="This field must be completed!"
-                handleInput={handleInput}
-                handleClick={() => {
-                  setShowWarning3(false);
-                }}
-                handleBlur={(e) => {
-                  if (e.target.value === '') setShowWarning3(true);
-                }}
-                showWarning={showWarning3}
+                register={register}
+                error={errors.phone?.message}
               />
             </div>
             <div>
               <Input
                 labelText="Email"
                 name="email"
-                inputValue={employeeInput.email}
+                type="text"
                 placeholder="Email"
-                warningMsg="This field must be completed!"
-                handleInput={handleInput}
-                handleClick={() => {
-                  setShowWarning4(false);
-                }}
-                handleBlur={(e) => {
-                  if (e.target.value === '') setShowWarning4(true);
-                }}
-                showWarning={showWarning4}
+                register={register}
+                error={errors.email?.message}
               />
             </div>
             <div>
               <Input
                 labelText="Password"
                 name="password"
-                inputValue={employeeInput.password}
+                type="password"
                 placeholder="Password"
-                warningMsg="This field must be completed!"
-                handleInput={handleInput}
-                handleClick={() => {
-                  setShowWarning5(false);
-                }}
-                handleBlur={(e) => {
-                  if (e.target.value === '') setShowWarning5(true);
-                }}
-                showWarning={showWarning5}
+                register={register}
+                error={errors.password?.message}
               />
             </div>
             <div>
@@ -224,7 +202,7 @@ const FormEmployee = (props) => {
               <Button type={('submit', styles.employeeBtnEdit)}>Create</Button>
               <Button
                 type={styles.employeeBtnEdit}
-                handleClick={() => props.history.push('/employees')}
+                handleClick={() => props.history.push('/employee')}
               >
                 Return
               </Button>
@@ -243,7 +221,7 @@ const FormEmployee = (props) => {
               if (!showButton && successEmployee) {
                 setShowButton(true);
                 setSuccessEmployee(false);
-                props.history.push('/employees');
+                props.history.push('/employee');
               } else {
                 setShowButton(true);
                 setSuccessEmployee(false);
