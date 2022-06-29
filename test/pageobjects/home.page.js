cf242f5be8dee5eba47415a56232c715c1c3ea0a
Sebastián Ronchetti <ronchettisebastian@gmail.com>
2022-06-28 23:53:50 -0300

    Employee e2e almost final version 2

--- test/pageobjects/home.page.js
+++ test/pageobjects/home.page.js
@@ -1,16 +1,20 @@
 class homePage {
-    get loginBtn() {return $('//*[@id="root"]/div/header/div/div/a[2]')};
-    get signUpBtn() {return $('//*[@id="root"]/div/header/div/div/a[1]')};
-    
-    async clickOn (btnName) {
-        switch (btnName) {
-            case 'login':
-                await this.navBtn.click();
-                break;
-            case 'signup':
-                await this.signUpBtn.click();
-        }
-    };
-};
+  get loginBtn() {
+    return $('//*[@id="root"]/div/header/div/div/a[2]');
+  }
+  get signUpBtn() {
+    return $('//*[@id="root"]/div/header/div/div/a[1]');
+  }
+
+  async clickOn(btnName) {
+    switch (btnName) {
+      case 'login':
+        await this.navBtn.click();
+        break;
+      case 'signup':
+        await this.signUpBtn.click();
+    }
+  }
+}
 
 module.exports = new homePage();
--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -1,20 +1,28 @@
 class loginPage {
-    get emailInput() {return $('//*[@id="root"]/div/div/div/div[2]/form/div[1]/div[1]/input')};
-    get passwordInput() {return $('//*[@id="root"]/div/div/div/div[2]/form/div[1]/div[2]/input')};
+  get emailInput() {
+    return $('//*[@id="root"]/div/div/div/div[2]/form/div[1]/div[1]/input');
+  }
+  get passwordInput() {
+    return $('//*[@id="root"]/div/div/div/div[2]/form/div[1]/div[2]/input');
+  }
 
-    get homeBtn() {return $('//*[@id="root"]/div/header/div/a')};
-    get loginBtn() {return $('//*[@id="root"]/div/div/div/div[2]/form/div[2]/button')};
+  get homeBtn() {
+    return $('//*[@id="root"]/div/header/div/a');
+  }
+  get loginBtn() {
+    return $('//*[@id="root"]/div/div/div/div[2]/form/div[2]/button');
+  }
 
-    async fillInput (inputName, inputValue) {
-        switch (btnName) {
-            case 'email':
-                await this.emailInput.setValue(inputValue);
-                break;
-            case 'password':
-                await this.passwordInput.setValue(inputValue);
-                break;
-        };
-    };
+  async fillInput(inputName, inputValue) {
+    switch (inputName) {
+      case 'email':
+        await this.emailInput.setValue(inputValue);
+        break;
+      case 'password':
+        await this.passwordInput.setValue(inputValue);
+        break;
+    }
+  }
 }
 
 module.exports = new loginPage();
