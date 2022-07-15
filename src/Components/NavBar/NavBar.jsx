56095402e80aa5ab114afb944a638a220325b266
Lucas De Lorenzi <lucasrauldelorenzi@gmail.com>
2022-07-15 14:41:09 -0300

    BaSP-TG-204: remove comment code in NavBar component

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -10,38 +10,7 @@ const NavBar = () => {
   if (!role) {
     return null;
   }
-  // const { routes } = props;
-  // switch (role) {
-  //   case 'EMPLOYEE':
-  //     return (
-  //       <nav className={styles.navbar}>
-  //         <div className={styles.userInfo}>
-  //           <img src={userIcon} className={styles.userImg} />
-  //           <div className={styles.userName}>user name</div>
-  //           <div className={styles.userRole}>Employee</div>
-  //         </div>
-  //         <div className={styles.menu}>menu</div>
-  //         <ul className={styles.routes}>
-  //           <li>
-  //             <Link className={styles.links} to="/employee/project">
-  //               projects
-  //             </Link>
-  //           </li>
-  //           <li>
-  //             <Link className={styles.links} to="/employee/timesheet">
-  //               timesheet
-  //             </Link>
-  //           </li>
-  //           <li>
-  //             <Link className={styles.links} to="/employee/profile">
-  //               personal Information
-  //             </Link>
-  //           </li>
-  //         </ul>
-  //       </nav>
-  //     );
 
-  //   case 'ADMIN':
   return (
     <nav className={styles.navbar}>
       <div className={styles.userInfo}>
@@ -63,31 +32,6 @@ const NavBar = () => {
       </ul>
     </nav>
   );
-
-  //     case 'SUPERADMIN':
-  //       return (
-  //         <nav className={styles.navbar}>
-  //           <div className={styles.userInfo}>
-  //             <img src={userIcon} className={styles.userImg} />
-  //             <div className={styles.userName}>user name</div>
-  //             <div className={styles.userRole}>user role</div>
-  //           </div>
-  //           <div className={styles.menu}>menu</div>
-  //           <ul className={styles.routes}>
-  //             <li>
-  //               <Link className={styles.links} to="/super-admins">
-  //                 admins
-  //               </Link>
-  //             </li>
-  //           </ul>
-  //         </nav>
-  //       );
-
-  //     case 'PM':
-  //       return props.history.push('/employee');
-  //     default:
-  //       break;
-  //   }
 };
 
 export default withRouter(NavBar);
