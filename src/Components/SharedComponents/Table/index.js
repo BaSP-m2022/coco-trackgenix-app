b2ed7d29914ee7b51698d56b0a05a20489a1d18c
Marco Migliaro <marcomig96@gmail.com>
2022-07-20 19:32:25 -0300

    BaSP-TG-112: Added button to see members in PM and admin tables

--- src/Components/SharedComponents/Table/index.js
+++ src/Components/SharedComponents/Table/index.js
@@ -5,8 +5,30 @@ import Modal from '../Modal/Modal';
 
 const Table = ({ data, headers, children, handleEdit, deleteItem }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isOpenMembers, setIsOpenMembers] = useState(false);
   const [rowMember, setRow] = useState();
-  if (window.location.pathname === '/employee/project') {
+
+  const membersBtn = (row, header) => {
+    // row[header].length;
+    // console.log(row.members);
+    const member1 = row.members[0].employee.firstName;
+    const member2 = row.members[1].employee.firstName;
+    const member3 = row.members[2].employee.firstName;
+    console.log(member1 + member2 + member3);
+    return (
+      <Button
+        type={styles.stylesBtn}
+        handleClick={() => {
+          setIsOpenMembers(true);
+          setRow(row._id);
+        }}
+      >
+        {row[header].length} Members
+      </Button>
+    );
+  };
+
+  if (window.location.pathname === '/employee/projects') {
     return (
       <div className={styles.container}>
         <div className={styles.buttonAdd}>{children}</div>
@@ -29,7 +51,7 @@ const Table = ({ data, headers, children, handleEdit, deleteItem }) => {
                   {headers.map((header, index) => {
                     return (
                       <td className={styles.data} key={index}>
-                        {header === 'members' ? row[header].length : row[header]}
+                        {header === 'members' ? membersBtn(row, header) : row[header]}
                       </td>
                     );
                   })}
@@ -53,6 +75,27 @@ const Table = ({ data, headers, children, handleEdit, deleteItem }) => {
               handleClick={() => {
                 deleteItem(rowMember);
                 setIsOpen(false);
+              }}
+            >
+              Confirm
+            </Button>
+          </div>
+        </Modal>
+        <Modal showModal={isOpenMembers} closeModal={() => setIsOpenMembers(false)}>
+          <h2>Members</h2>
+          <div>
+            {/* <p>Are you sure you want to delete this item?</p>
+            <p>You will not be able to recover it</p> */}
+          </div>
+          <div>
+            <Button type={styles.deleteModalBtn} handleClick={() => setIsOpenMembers(false)}>
+              Cancel
+            </Button>
+            <Button
+              type={styles.stylesModalBtn}
+              handleClick={() => {
+                // deleteItem(rowMember);
+                setIsOpenMembers(false);
               }}
             >
               Confirm
