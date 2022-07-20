94d54c9b81648296f65c45c19a834cfa1999a305
Lucas De Lorenzi <lucasrauldelorenzi@gmail.com>
2022-07-20 17:39:58 -0300

    BaSP-TG-204: add currentUser function in NavBar to display it there

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,26 +1,40 @@
-import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link, withRouter } from 'react-router-dom';
 import userIcon from '../../Assets/user.svg';
 import styles from './navBar.module.css';
 
 const NavBar = () => {
-  const routes = useSelector((state) => state.login.routes);
+  const currentUsr = useSelector((state) => state.login);
+  const users = useSelector((state) => state);
+
   const role = sessionStorage.getItem('role');
   if (!role) {
     return null;
   }
 
+  const currentUser = () => {
+    if (role === 'PM' || role === 'EMPLOYEE') {
+      const employee = users.employee.list.find(
+        (employee) => employee.email === currentUsr.userEmail
+      );
+      return `${employee?.firstName} ${employee?.lastName}`;
+    } else if (role === 'ADMIN') {
+      const admin = users.admin.list.find((admin) => admin.email === currentUsr.userEmail);
+      return `${admin?.name} ${admin?.lastName}`;
+    }
+    return 'Welcome';
+  };
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.userInfo}>
         <img src={userIcon} className={styles.userImg} />
-        <div className={styles.userName}>user name</div>
-        <div className={styles.userRole}>user role</div>
+        <div className={styles.userName}>{currentUser()}</div>
+        <div className={styles.userRole}>{role}</div>
       </div>
       <div className={styles.menu}>menu</div>
       <ul className={styles.routes}>
-        {routes.map((element, index) => {
+        {currentUsr.routes.map((element, index) => {
           return (
             <li key={index}>
               <Link className={styles.links} to={element.path}>
